a932974a4f578524550259ed9ea2c14fd35517a5
Santiago Rojas <santirojastr12@gmail.com>
2023-11-16 15:00:22 -0500

    Creacion del modelo Usuario

---
+++ SistemaGestionBiblioteca/Models/Usuario.cs
@@ -0,0 +1,11 @@
+﻿namespace SistemaGestionBiblioteca.Models
+{
+    public class Usuario
+    {
+        public int UsuarioId { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string CorreoElectronico { get; set; }
+        public string Contraseña {  get; set; }
+    }
+}
