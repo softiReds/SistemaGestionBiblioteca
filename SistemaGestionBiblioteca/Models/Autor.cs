a15626dc715a3417c37da3e439154724181a74e8
Santiago Rojas <santirojastr12@gmail.com>
2023-11-16 14:57:42 -0500

    Creacion del modelo Autor

---
+++ SistemaGestionBiblioteca/Models/Autor.cs
@@ -0,0 +1,8 @@
+﻿namespace SistemaGestionBiblioteca.Models
+{
+    public class Autor
+    {
+        public int AutorId {  get; set; }
+        public string Nombre { get; set; }
+    }
+}
---
+++ SistemaGestionBiblioteca/Models/Libro.cs
@@ -0,0 +1,13 @@
+﻿namespace SistemaGestionBiblioteca.Models
+{
+    public class Libro
+    {
+        public int LibroId { get; set; }
+        public string Titulo {  get; set; }
+        public int AutorId {  get; set; }
+        public int CantidadDisponivle { get; set; }
+        public DateTime FechaPublicacion { get; set; }
+
+        public virtual Autor Autor { get; set; }
+    }
+}
