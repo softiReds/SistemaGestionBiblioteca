f240ce88fac94ab8fa8b615f970fc9a4f73166bc
Santiago Rojas <santirojastr12@gmail.com>
2023-11-16 15:04:02 -0500

    Creacion del modelo Reserva

---
+++ SistemaGestionBiblioteca/Models/Reserva.cs
@@ -0,0 +1,20 @@
+﻿namespace SistemaGestionBiblioteca.Models
+{
+    public class Reserva
+    {
+        public int ReservaId { get; set; }
+        public int LibroId {  get; set; }
+        public int UsuarioId {  get; set; }
+        public DateTime FechaReserva {  get; set; }
+        public DateTime FechaDevolucion { get; set; }
+        public EstadoReserva Estado { get; set; }
+    }
+
+    public enum EstadoReserva
+    {
+        Pendiente,
+        Activada,
+        Vencida,
+        Devuelta
+    }
+}
