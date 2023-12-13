64e31a391fcbc652353e2bd127c0a3723a92ad75
Santiago Rojas <santirojastr12@gmail.com>
2023-12-13 17:03:25 -0500

    Creacion de modelo para Tipos de usuario

---
+++ SistemaGestionBiblioteca/API/SistemaGestionBibliotecaAPI/Models/TipoUsuario.cs
@@ -0,0 +1,10 @@
+﻿namespace SistemaGestionBibliotecaAPI.Models
+{
+    public class TipoUsuario
+    {
+        public Guid TipoUsuarioId { get; set; }
+        public string Descripcion { get; set; }
+
+        public virtual ICollection<Usuario> Usuarios { get; set; }
+    }
+}
