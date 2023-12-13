b8fb99dabe48bfacaa059965c74b6e371abc6c34
Santiago Rojas <santirojastr12@gmail.com>
2023-12-13 17:03:32 -0500

    Creacion de modelo para usuario

---
+++ SistemaGestionBiblioteca/API/SistemaGestionBibliotecaAPI/Models/Usuario.cs
@@ -0,0 +1,14 @@
+﻿namespace SistemaGestionBibliotecaAPI.Models
+{
+    public class Usuario
+    {
+        public Guid UsuarioId { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string CorreoElectronico { get; set; }
+        public string Contraseña {  get; set; }
+        public Guid TipoUsuarioId { get; set; }
+
+        public virtual TipoUsuario TipoUsuario { get; set; }
+    }
+}
