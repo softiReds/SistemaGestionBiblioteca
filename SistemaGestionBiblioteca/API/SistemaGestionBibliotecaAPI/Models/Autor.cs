3a3e8966a24cdac290213ec4d624d60c3bb6d7e6
Santiago Rojas <santirojastr12@gmail.com>
2023-12-13 16:57:24 -0500

    .vs

---
+++ SistemaGestionBiblioteca/API/SistemaGestionBibliotecaAPI/Models/Autor.cs
@@ -0,0 +1,8 @@
+﻿namespace SistemaGestionBibliotecaAPI.Models
+{
+    public class Autor
+    {
+        public Guid AutorId {  get; set; }
+        public string AutorNombre { get; set; }
+    }
+}
