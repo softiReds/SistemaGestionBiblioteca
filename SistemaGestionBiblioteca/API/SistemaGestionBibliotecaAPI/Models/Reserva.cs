143f0f7faba3e49aac3a21559a0cca944bef980e
Santiago Rojas <santirojastr12@gmail.com>
2023-12-13 17:07:59 -0500

    Creacion de modelo para reservas

---
+++ SistemaGestionBiblioteca/API/SistemaGestionBibliotecaAPI/Models/Reserva.cs
@@ -0,0 +1,16 @@
+﻿namespace SistemaGestionBibliotecaAPI.Models
+{
+    public class Reserva
+    {
+        public Guid ReservaId { get; set; }
+        public Guid LibroId { get; set; }
+        public Guid UsuarioId {  get; set; }
+        public DateTime FechaReserva { get; set; }
+        public DateTime FechaDevolucion { get; set; }
+        public Guid EstadoReservaId { get; set; }
+
+        public virtual Libro Libro { get; set; }
+        public virtual Usuario Usuario {  get; set; }
+        public virtual EstadoReserva EstadoReserva { get; set; }
+    }
+}
