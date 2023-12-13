d6f4d83b535bf58b0d909ea8407a1202a37df4eb
Santiago Rojas <santirojastr12@gmail.com>
2023-12-13 17:07:33 -0500

    Creacion de modelo para estados de reserva

---
+++ SistemaGestionBiblioteca/API/SistemaGestionBibliotecaAPI/Models/EstadoReserva.cs
@@ -0,0 +1,10 @@
+﻿namespace SistemaGestionBibliotecaAPI.Models
+{
+    public class EstadoReserva
+    {
+        public Guid EstadoReservaId {  get; set; }
+        public string Descripcion { get; set; }
+
+        public virtual ICollection<Reserva> Reservas { get; set; }
+    }
+}
