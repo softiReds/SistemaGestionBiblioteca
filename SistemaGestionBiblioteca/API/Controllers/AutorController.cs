30f26dcc04bce7baf3227a560033f7253683a84e
Santiago Rojas <santirojastr12@gmail.com>
2023-12-12 21:42:16 -0500

    Se crea el controlador para autores

---
+++ SistemaGestionBiblioteca/API/Controllers/AutorController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaGestionBibliotecaAPI.Services;
+
+namespace SistemaGestionBibliotecaAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AutorController : ControllerBase
+    {
+        private readonly IAutorService _service;
+
+        public AutorController(IAutorService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_service.Get());
+        }
+    }
+}
